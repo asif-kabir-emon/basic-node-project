fd468cd2bfcafbd307268279c2d3fc6163bb5105
Asif Kabir Emon <asif.kabir@coolriots.io>
2023-12-03 21:35:02 +0800

    Add error handling

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ async function main() {
             console.log(`Server is running on port ${config.port}`);
         });
     } catch (error) {
-        console.log(error);
+        console.log('Failed to connect to database');
     }
 }
 
